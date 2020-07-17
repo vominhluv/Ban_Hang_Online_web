22882461e1837a01b9ce632fe8217d2c27f7950f
TruongTam <you@example.com>
2020-07-17 15:11:32 +0700

    homepage

---
+++ homepage.js
@@ -0,0 +1,11 @@
+function showfirst(a,b,conn) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "first(0,8,$conn)", true);
+    xhttp.send();
+}
