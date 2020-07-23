1126636adec065eac5136669d3c03b9de2cb1bd1
tranxuanthin <nhattranlinh553@gmail.com>
2020-07-23 22:08:33 +0700

    thìn fix database. ae coi link nao không trong connect thì fix gấp

--- connet.php
+++ connet.php
@@ -2,7 +2,7 @@
     $servername = "127.0.0.1";
     $username = "root";
     $password = "";
-    $database  = "qlao";
+    $database  = "database_web_ban_hang";
     $conn = mysqli_connect($servername, $username, $password,$database) or die("lỗi");
     mysqli_set_charset($conn, 'UTF8');
 ?>
