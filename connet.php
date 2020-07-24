a3d4257409440f1e89bf4b575a2e11ab8a82f90d
Minh Luy <vominhluy1@gmail.com>
2020-07-24 10:44:45 +0700

    Luy

--- connet.php
+++ connet.php
@@ -2,7 +2,7 @@
     $servername = "127.0.0.1";
     $username = "root";
     $password = "";
-    $database  = "database_web_ban_hang";
+    $database  = "qlao";
     $conn = mysqli_connect($servername, $username, $password,$database) or die("lỗi");
     mysqli_set_charset($conn, 'UTF8');
 ?>
